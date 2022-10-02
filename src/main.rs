532ae4f199fe6d8335e6d5513997cb19dcd198b9
zll600 <3400692417@qq.com>
2022-10-02 14:54:30 +0800

    Add cargo toolchains

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
 use std::io::{self, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    dbg!(silent);
     let mut total_bytes = 0;
     loop {
         let mut buffer = [0; CHUNK_SIZE];
@@ -11,8 +14,11 @@ fn main() {
             Ok(x) => x,
             Err(_) => break,
         };
-        total_bytes += num_read;
+        // total_bytes += num_read;
+        dbg!(total_bytes += num_read);
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
-    eprintln!("{}", total_bytes);
+    if !silent {
+        eprintln!("{}", total_bytes);
+    }
 }
