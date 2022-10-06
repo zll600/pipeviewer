d236734b3e24221055fd3abbeddb1c698cc3a3f6
zll600 <3400692417@qq.com>
2022-10-06 15:06:18 +0800

    Add Timer struct

--- src/stats.rs
+++ src/stats.rs
@@ -1,23 +1,23 @@
 use crossbeam::channel::Receiver;
 use std::io::Result;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start_time = Instant::now();
-    let mut last_instant = Instant::now();
+    let mut timer = Timer::new();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
-        let now = Instant::now();
-        let rate_per_second = num_bytes as f64 / (now - last_instant).as_secs_f64();
-        last_instant = now;
+        timer.update();
+        let rate_per_second = num_bytes as f64 / timer.delta.as_secs_f64();
 
         total_bytes += num_bytes;
-        if !silent {
+        if !silent && timer.ready {
+            timer.ready = false;
             eprint!(
                 "\r{} {} [{:.0}b/s]",
                 total_bytes,
-                start_time.elapsed().as_secs(),
+                start_time.elapsed().as_secs().as_time(),
                 rate_per_second
             );
         }
@@ -29,4 +29,47 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+trait TimeOutput {
+    fn as_time(&self) -> String;
+}
+
+impl TimeOutput for u64 {
+    fn as_time(&self) -> String {
+        let (hours, left) = (*self / 3600, *self % 3600);
+        let (minutes, seconds) = (left / 60, left % 60);
+        format!("{}:{:02}:{:02}", hours, minutes, seconds)
+    }
+}
+
+struct Timer {
+    last_instant: Instant,
+    pub delta: Duration,
+    period: Duration,
+    countdown: Duration,
+    pub ready: bool,
+}
+
+impl Timer {
+    pub fn new() -> Self {
+        let now = Instant::now();
+        Self {
+            last_instant: now,
+            delta: Duration::default(),
+            period: Duration::from_millis(1000),
+            countdown: Duration::default(),
+            ready: true,
+        }
+    }
+
+    pub fn update(&mut self) {
+        let now = Instant::now();
+        self.delta = now - self.last_instant;
+        self.last_instant = now;
+        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
+            self.ready = true;
+            self.period
+        });
+    }
 }
